7e2cd0364ebc6b6368825eaa55f54c06aa4b220d
jash-git <jash.liao@gmail.com>
2017-04-06 09:50:20 +0800

    modified project_code/DLL_path/DLL_path_note.txt
    add Qt_009 project

---
+++ project_code/Qt_009/main.cpp
@@ -0,0 +1,32 @@
+#include <QApplication>
+#include <QtGui>
+#include <QTCore>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc,argv);
+    QWidget *windows=new QWidget;
+    windows->setWindowTitle("Qt_009");
+
+    ///
+    QGridLayout *Layout = new QGridLayout;
+
+    QLabel *Label_01=new QLabel("Name");
+    QLineEdit *LineEdit_01=new QLineEdit;
+
+    QLabel *Label_02=new QLabel("Name");
+    QLineEdit *LineEdit_02=new QLineEdit;
+
+    QPushButton *Button_01=new QPushButton("OK");
+
+    Layout->addWidget(Label_01,0,0);//x,y
+    Layout->addWidget(LineEdit_01,0,1);//x,y
+    Layout->addWidget(Label_02,1,0);//x,y
+    Layout->addWidget(LineEdit_02,1,1);//x,y
+    Layout->addWidget(Button_01,2,0,1,2);//x,y,y_width,x_width
+    windows->setLayout(Layout);
+    ///
+
+    windows->show();
+    return app.exec();
+}
